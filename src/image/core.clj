e5db80c1ddc0a86d1863236a23c4a6dc341dc796
Jessica Appelbaum <jessica.appelbaum@thinktopic.com>
2016-10-24 14:07:26 -0600

    [core.clj] made home into a timesheet table

--- src/image/core.clj
+++ src/image/core.clj
@@ -11,55 +11,92 @@
 
 (def user-db* (atom {}))
 
-(defn create-user
-  [name]
-  (swap! user-db* #(assoc % name {:check-ins []
-                                  :check-outs []}))
-  (p/pprint @user-db*))
-
-(defn checked-in?
-  [name]
-  (if (> (count (:check-ins (get @user-db* name)))
-         (count (:check-outs (get @user-db* name))))
-    (swap! user-db* #(assoc % :checked-in? true))
-    (swap! user-db* #(assoc % :checked-in? false)))
-  (p/pprint @user-db*))
+;;Middleware
 
 (defn wrap-time-in-request [handler]
   (fn [request]
     (handler (assoc request :current-time (System/currentTimeMillis)))))
 
+;;Model Functions
+
+(defn create-user
+  [name]
+  (if (empty? (get @user-db* name))
+    (swap! user-db* #(assoc % name {:clock-ins []
+                                    :clock-outs []}))
+    user-db*))
+
+(defn clocked-in?
+  [name]
+  (> (count (:clock-ins (get @user-db* name)))
+     (count (:clock-outs (get @user-db* name)))))
+
+(defn clock-in
+  [name]
+  (swap! user-db* (fn [state] (let [new-clock-ins (conj (:clock-ins (get state name)) (System/currentTimeMillis))]
+                                (assoc-in state [name :clock-ins] new-clock-ins))))
+  (p/pprint (str "DEREF USER-DB CLOCK-IN " @user-db*)))
+
+(defn clock-out
+  [name]
+  (swap! user-db* (fn [state] (let [new-clock-outs (conj (:clock-outs (get state name)) (System/currentTimeMillis))]
+                                (assoc-in state [name :clock-outs] new-clock-outs))))
+  (p/pprint (str "DEREF USER-DB CLOCK-OUT " @user-db*)))
+
+(defn millis->date
+  [time]
+  (format-date (java.util.Date. time)))
+
+#_(assoc-in state [name :clock-ins] (conj (:clock-ins (get state name)) (System/currentTimeMillis)))
+#_(swap! user-db* #(assoc % :clocked-in? true))
+
+;;Routes
+
 (defroutes image
   (GET "/" []
        (html
-         [:div "main page"]
-         [:a {:href "/login"} "go to login"]))
+         [:h1 "login"]
+         [:form {:method "post"
+                 :action "/"}
+          "username: "
+          [:div [:input {:type "text"
+                         :name "user"}]]
+          [:div [:input {:type "submit"}]]]))
 
-  (GET "/howdy/:name" [name] (str "Howdy, " name "!"))
-
-  (GET "/login" []
-       (html [:form {:method "post"
-                     :action "/login"}
-              "username: "
-              [:div [:input {:type "text"
-                             :name "user"}]]
-              [:div [:input {:type "submit"}]]]))
-
-  (POST "/login" request
+  (POST "/" request
         (let [user (get (:params request) "user")]
           (create-user user)
-          (html [:div
-                 [:span (str "welcome to clock-in-n-out " user "!")]
-                 [:form {:method "post"
-                         :action "/home"}
-                  [:input {:type "submit"
-                           :value "clock in"}]]])))
+          (clocked-in? user)
+          (html
+            [:h1 "clock-in-n-out"]
+            [:span (str "welcome to clock-in-n-out " user "!")]
+            [:form {:method "post"
+                    :action "/home"}
+             [:input {:type "hidden"
+                      :name "user"
+                      :value user}]
+             (if (clocked-in? user)
+               [:input {:type "submit"
+                        :value "clock out"}]
+               [:input {:type "submit"
+                        :value "clock in"}])])))
 
   (POST "/home" request
         (let [user (get (:params request) "user")]
-          (checked-in? user)
-          (html [:div
-                 [:div (str "time in: ")]])))
+          (if (clocked-in? user)
+            (clock-out user)
+            (clock-in user))
+          (let [user-map (get @user-db* user)]
+            (html
+              [:h1 "timesheet"]
+              [:table
+               [:thead
+                [:tr [:th "time-in"] [:th "time-out"] [:th "hours"]]]
+               [:tbody
+                [:tr
+                 [:td (millis->date (last (:clock-ins user-map)))]
+                 (when (not-empty (:clock-outs user-map))
+                   [:td (millis->date (last (:clock-outs user-map)))])]]]))))
 
   (compojure.route/not-found "Page not found"))
 
